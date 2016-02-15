7a50365d94468566ab993feac3bf6820ff57fece
njoodnj <aidmenp@gmail.com>
2016-02-15 18:35:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/njood/es/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.njood.es;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends Activity {
+
+    EditText id, password;
+    String Id, Password;
+    Context ctx=this;
+    String ID=null, NAME=null, PASSWORD=null, EMAIL=null, ADDRESS=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+        id = (EditText) findViewById(R.id.NationalID);
+        password = (EditText) findViewById(R.id.passwordHP);
+    }
+
+    public void register_register(View v){
+        startActivity(new Intent(this,Register.class));
+    }
+
+    public void main_login(View v){
+        Id = id.getText().toString();
+        Password = password.getText().toString();
+        BackGround b = new BackGround();
+        b.execute(Id, Password);
+    }
+
+    class BackGround extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String id = params[0];
+            String password = params[1];
+            String data="";
+            int tmp;
+
+            try {
+                URL url = new URL("http://10.0.2.2/ES/login.php");
+                String urlParams = "r_id="+id+"&r_password="+password;
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setDoOutput(true);
+                OutputStream os = httpURLConnection.getOutputStream();
+                os.write(urlParams.getBytes());
+                os.flush();
+                os.close();
+
+                InputStream is = httpURLConnection.getInputStream();
+                while((tmp=is.read())!=-1){
+                    data+= (char)tmp;
+                }
+
+                is.close();
+                httpURLConnection.disconnect();
+
+                return data;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return "Exception: "+e.getMessage();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "Exception: "+e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            try {
+                JSONObject root = new JSONObject(s);
+                JSONObject user_data = root.getJSONObject("user_data");
+                ID = user_data.getString("r_id");
+                NAME = user_data.getString("r_name");
+                PASSWORD = user_data.getString("r_password");
+                EMAIL = user_data.getString("r_email");
+                ADDRESS = user_data.getString("r_address");
+            } catch (JSONException e) {
+                e.printStackTrace();
+
+            }
+
+            Intent i = new Intent(ctx, User_Home.class);
+            i.putExtra("r_id", ID);
+            i.putExtra("r_name", NAME);
+            i.putExtra("r_password", PASSWORD);
+            i.putExtra("r_email", EMAIL);
+            i.putExtra("r_address", ADDRESS);
+            startActivity(i);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/njood/es/User_Home.java
@@ -0,0 +1,46 @@
+package com.example.njood.es;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class User_Home extends Activity {
+
+    String id , name, email, address, welcome;
+    TextView idTV ,nameTV, emailTV , addressTV, welcomeTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user__home);
+
+        idTV = (TextView) findViewById(R.id.home_id);
+        nameTV = (TextView) findViewById(R.id.home_name);
+        emailTV = (TextView) findViewById(R.id.home_email);
+        addressTV = (TextView) findViewById(R.id.home_address);
+        welcomeTV = (TextView) findViewById(R.id.welcomename);
+
+        id = getIntent().getStringExtra("r_id");
+        name = getIntent().getStringExtra("r_name");
+        email = getIntent().getStringExtra("r_email");
+        address = getIntent().getStringExtra("r_address");
+        welcome = getIntent().getStringExtra("r_name");
+
+
+        idTV.setText("ID No:"+id);
+        nameTV.setText("Name: "+name);
+        emailTV.setText("Email:"+email);
+        addressTV.setText("Address:"+address);
+        welcomeTV.setText("Welcome "+name);
+    }
+}
+
+
